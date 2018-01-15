e481d424631da6b1f0d20912ca4b8e8964094d33
kiran <me.kirankg@gmail.com>
2018-01-15 22:27:04 +0530

    routing to pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { CreatequotationComponent } from './quotation/createquotation.component';
+import { FormStyle } from '@angular/common/src/i18n/locale_data_api';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatequotationComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+        { path:'createquotation',component:CreatequotationComponent },
+        { path:'welcome',component:WelcomeComponent }    ,
+        { path:'',component:WelcomeComponent,pathMatch:'full' },
+        { path:'**',component:WelcomeComponent ,pathMatch:'full'}
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
