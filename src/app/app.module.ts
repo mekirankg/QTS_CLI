d60218455f3857026be8a3717ad2cdb5a38037d4
kiran <me.kirankg@gmail.com>
2018-01-23 22:41:20 +0530

    login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule,  HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 
@@ -11,7 +11,16 @@ import { FormStyle } from '@angular/common/src/i18n/locale_data_api';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ListquotationComponent } from './quotation/listquotation.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
 
+
+import { fakeBackendProvider } from './_helpers/index';
+import { AlertComponent } from './_directives/index';
+import { AuthGuard } from './_guards/index';
+import { JwtInterceptor } from './_helpers/index';
+import { AlertService, AuthenticationService, UserService } from './_services/index';
+import { HomeComponent } from './home/index';
+import { RegisterComponent } from './register/index';
 
 @NgModule({
   declarations: [
@@ -19,7 +28,11 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     CreatequotationComponent,
     WelcomeComponent,
     ListquotationComponent,
-    SidebarComponent
+    SidebarComponent,    
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -29,13 +42,30 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
       [
         { path:'createquotation',component:CreatequotationComponent },
         { path:'listquotation',component:ListquotationComponent },
-        { path:'welcome',component:WelcomeComponent }    ,
+        { path:'welcome',component:WelcomeComponent },
+        {path:'login',component:LoginComponent},
+        { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+        { path: 'register', component: RegisterComponent },
         { path:'',component:WelcomeComponent,pathMatch:'full' },
         { path:'**',component:WelcomeComponent ,pathMatch:'full'}
       ]
     )
   ],
-  providers: [],
+  
+providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+        provide: HTTP_INTERCEPTORS,
+        useClass: JwtInterceptor,
+        multi: true
+    },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
